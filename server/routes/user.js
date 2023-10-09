00b8af4b9aeba536b8c840338031c9aaab2d205f
Erik Nunes <erikvixx@gmail.com>
2023-10-09 11:32:48 -0300

    fix: #2 Modularization of routes

---
+++ server/routes/user.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const userModel = require('../models/UserModel')
+
+router.use(express.json());
+
+router.get("/users", async (req, res) => {
+  try {
+    const userData = await userModel.find();
+    res.json(userData);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/user", async (req,res) =>{
+  try {
+    const payload = req.body
+    const userData = await userModel.create({ ...payload })
+    res.send(userData)
+  } catch (error) {
+    res.status(500).json({message : error.message})
+  }
+})
+router.put("/user/:_id", async (req, res) => {
+    const { _id } = req.params;
+    const payload = req.body;
+    try {
+        const userData = await userModel.findOneAndUpdate(
+          { _id },
+          { ...payload },
+          {
+            new: true,
+          }
+        );
+        res.send(userData);
+    } catch (error) {
+      res.send(error);
+    }
+  });
+  
+ router.delete("/user/:_id", async (req, res) => {
+    const { _id } = req.params;
+    try {
+      await userModel.deleteOne({ _id }, { new: true });
+      res.status(200).send();
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+    }
+  });
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,77 +2,16 @@ const express = require("express");
 const ConnectToDataBase = require("../server/database/connect");
 const app = express();
 const port = process.env.PORT || 5000;
-const UserModel = require("./models/UserModel");
+const baseUrl = "/api";
 const cors = require("cors");
+let user = require('./routes/user')
 require("dotenv").config();
 
+app.use(baseUrl, user)
 ConnectToDataBase();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/users", async (req, res) => {
-  try {
-    const users = await UserModel.find();
-    res.json({ users });
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-app.post("/api/user", async (req, res) => {
-  try {
-    const payload = req.body;
-    if (
-      !payload.email &&
-      !payload.firstname &&
-      !payload.lastname &&
-      !payload.password
-    ) {
-      res
-        .status(422)
-        .json({ message: "Please provide all the required fields" });
-    }
-    const userId = await UserModel.create({ ...payload });
-    res.send(userId);
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-app.put("/api/user/:_id", async (req, res) => {
-  const { _id } = req.params;
-  const payload = req.body;
-  try {
-    if (
-      !payload.email &&
-      !payload.firstname &&
-      !payload.lastname &&
-      !payload.password
-    ) {
-      const userId = await UserModel.findOneAndUpdate(
-        { _id },
-        { ...payload },
-        {
-          new: true,
-        }
-      );
-      res.send(userId);
-    }
-  } catch (error) {
-    res.send(error);
-  }
-});
-
-app.delete("/api/user/:_id", async (req, res) => {
-  const { _id } = req.params;
-  try {
-    await UserModel.deleteOne({ _id }, { new: true });
-    res.status(200).send();
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
 
 app.listen(port, () => {
   console.log(`Servidor Express está rodando na porta ${port}`);
